14d4717a869546d6a218602bf2ad22626da40ad7
Max <raskolbrd@gmail.com>
2023-02-06 05:51:32 +0200

    Lite version

--- keyboards/main_menu.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher, types
-
-
-async def set_main_menu(dp: Dispatcher):
-    main_menu_commands = [
-        types.BotCommand(command='/info', description='Info'),
-    ]
-    await dp.bot.set_my_commands(main_menu_commands)
