85db738d547936786b6f9c86139befcc57d9d1e2
Max <raskolbrd@gmail.com>
2023-02-08 12:01:59 +0200

    little edit

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -2,7 +2,8 @@ LEXICON_EN = {'start': '🔍This bot can collect telegram users data just in few
                        '📖This process calls "parsing". To get familiar with it press "INFO"\n'
                        '🆕If you never used this bot, read info, then come back and start "SESSION"\n'
                        'Already registered? Press "parse"',
-              'instructions': '1. Login on https://my.telegram.org\n'
+              'instructions': '0. Disable 2FA (two-step verification) in your telegram Privacy Settings\n'
+                              '1. Login on https://my.telegram.org\n'
                               '2. Press "API development tools" and create a new app. Use any name and link\n'
                               '3. Send your App api_id, App api_hash and phone number\n'
                               'example: 12345678 a123b4cd968ab57cf529e770a9f17d28 +380123456789',
